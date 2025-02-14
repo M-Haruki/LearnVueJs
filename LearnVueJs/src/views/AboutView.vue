9f84835615ef0f53fb30ca1f9f27c4cddd5c7029
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-02-14 13:19:36 +0000

    ページ作成

--- LearnVueJs/src/views/AboutView.vue
+++ LearnVueJs/src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-view">
+    <h1>このサイトについて</h1>
+    <p>このサイトは、Vue.jsの学習用に作成されました。</p>
+    <p>Vue.js公式とは無関係のサイトです。</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about-view {
+  padding: 20px;
+}
+
+h1 {
+  color: #42b983;
+  font-size: 2rem;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
 }
 </style>
